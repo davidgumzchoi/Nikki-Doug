c42a575fad47f15e1358590fd3693587fd6eafb3
GumZ <davidgumzchoi@gmail.com>
2014-07-10 02:07:42 -0700

    initial redesigned commit

--- js/application.js
+++ js/application.js
@@ -1,15 +1,35 @@
-// Parallax Scrolling
-$(document).ready(function(){
-    $objWindow = $(window);
-    $('div[data-type="background"]').each(function(){
-        var $bgObj = $(this);
-        $(window).scroll(function() {
-            var yPos = -($objWindow.scrollTop() / $bgObj.data('speed')); 
-            var coords = '100% '+ yPos + 'px';
-            // Animate the background
-             $bgObj.css({backgroundPosition:coords});
-        });
-    });
+// // Parallax Scrolling
+// $(document).ready(function(){
+//     $objWindow = $(window);
+//     $('div[data-type="background"]').each(function(){
+//         var $bgObj = $(this);
+//         $(window).scroll(function() {
+//             var yPos = -($objWindow.scrollTop() / $bgObj.data('speed')); 
+//             var coords = '100% '+ yPos + 'px';
+//             // Animate the background
+//              $bgObj.css({backgroundPosition:coords});
+//         });
+//     });
+// });
+
+// Document Window Height
+$(function(){
+    var windowHeight = $(window).height();
+    var wrapperHeight = $('.wrapper').height();
+    if(windowHeight > wrapperHeight) {                            
+        $('.wrapper').css({'height':($(window).height())+'px'});
+    }                                                                               
+    $(window).resize(function(){
+        var windowHeight = $(window).height();
+        var wrapperHeight = $('.wrapper').height();
+        var differenceHeight = windowHeight - wrapperHeight;
+        var newHeight = wrapperHeight + differenceHeight;
+        var truecontentHeight = $('.parallax').height();
+        if(windowHeight > truecontentHeight) {
+            $('.wrapper').css('height', (newHeight)+'px');
+        }
+
+    })          
 });
 
 // Smooth Scrolling
