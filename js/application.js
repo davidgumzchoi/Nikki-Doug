0bfd154aed2ea2c259a19e9d4ded18296237e78f
GumZ <davidgumzchoi@gmail.com>
2014-07-14 18:57:15 -0700

    cleanup

--- js/application.js
+++ js/application.js
@@ -1,17 +1,3 @@
-// // Parallax Scrolling
-// $(document).ready(function(){
-//     $objWindow = $(window);
-//     $('div[data-type="background"]').each(function(){
-//         var $bgObj = $(this);
-//         $(window).scroll(function() {
-//             var yPos = -($objWindow.scrollTop() / $bgObj.data('speed')); 
-//             var coords = '100% '+ yPos + 'px';
-//             // Animate the background
-//              $bgObj.css({backgroundPosition:coords});
-//         });
-//     });
-// });
-
 // Document Window Height
 $(function(){
     var windowHeight = $(window).height();
